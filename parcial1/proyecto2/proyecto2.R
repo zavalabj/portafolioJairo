c2fc617aa4998ffdf7b0146d52efb6b8af78ab05
Regina del Pilar Berges Portillo <170300066@ucaribe.edu.mx>
2020-02-06 12:49:13 -0500

    Se añadió gráfica de num. estudiantes

--- parcial1/proyecto2/proyecto2.R
+++ parcial1/proyecto2/proyecto2.R
@@ -47,17 +47,26 @@ plot(df$BPS_Year_Founded, xaxs="i",  yaxs="i", main ="Número de escuelas fundad
 # SMMA_FA_susceptible_climate_change_2100, SMMA_FA_site_includes_Playgrounds
 
 #--------------------------------------------------------------------------------------------------
-# Código inútil
 
 # Cargar base de datos del Facilities and Educational Data for Boston Public Schools.
 setwd("C:/Users/regina/Desktop/6to Semestre/Minería de datos/portafolioJairo/parcial1/proyecto2")
 df <- read.csv("buildbps.csv", sep = ",")
+datos <- df %>% select(BPS_School_Name, BPS_Year_Founded, BPS_Year_Built, MSBA_Year_Reno, BPS_Address, SMMA_latitude, SMMA_longitude, SMMA_Typology, BPS_Property_Status, BPS_Total_GSF, SMMA_Site_SF, DOE_Total, GSF...Student.Value, BPS_KWH, BPS_Electric_Bill, BPS_Therms, BPS_Gas_Bill, BPS_Water_Bill, BPS_Total_Energy_Cost, Climate_Priority, SMMA_FA_Status, SMMA_FA_School_Enrollment, SMMA_FA_Enrollment_Building, SMMA_FA_Major_Investments, 
+                     SMMA_FA_Life_Safety, SMMA_FA_Building_suitability_school, SMMA_FA_susceptible_climate_change_now, SMMA_FA_susceptible_climate_change_2100, SMMA_FA_Electrical_Service, SMMA_FA_Security_Entry, SMMA_FA_Deterioration_Walls_Columns, SMMA_FA_Deterioration_Facade, SMMA_FA_Overall_Building_Condition, SMMA_FA_Emergency_Shelter, SMMA_FA_Community_Use_spaces, SMMA_FA_Neighborhood_Streets, SMMA_FA_Parking_Quality, SMMA_FA_DropOff_PickUp, 
+                     SMMA_FA_Walkways_Curbs_Sidewalks, SMMA_FA_MAABADA_Accessibility, SMMA_FA_Play_Areas, SMMA_FA_Walkable)
 
-# Obtener nombres de variables que contienen valores NA
-vars_to_exclude=df_status(df, print_results = F) %>% filter(p_na > 0) %>% .$variable
+# Distribución de estudiantes según el nivel de estudios
+high <- filter(datos, SMMA_Typology == "High School")
+special <- filter(datos, SMMA_Typology == "Special")
+elementary <- filter(datos, SMMA_Typology == "Elementary School")
+early <- filter(datos, SMMA_Typology == "Early Learning")
+k8 <- filter(datos, SMMA_Typology == "K-8")
+middle <- filter(datos, SMMA_Typology == "Middle School")
 
-# Quitar variables con un alto número de ceros
-vars_to_remove = filter(df_status(df), p_zeros > 30)  %>% .$variable
+# Cantidad de alumnos matrículados dependiendo en nivel de estudios
+cantidad_estudiantes <- c(sum(high$DOE_Total), sum(middle$DOE_Total), sum(special$DOE_Total),
+                          sum(k8$DOE_Total), sum(elementary$DOE_Total), sum(early$DOE_Total))
 
-# Conservar todas las columnas excepto aquellas presentes en el vector 'vars_to_remove' y 'vars_to_Exclude'
-df2 <- df %>% select(-vars_to_exclude, -vars_to_remove)
+matriculados <- data.frame(Grade_Typology=c("High School", "Middle School", "Special", "K-8", "Elementary School", "Early Learning"), Amount=cantidad_estudiantes)
+ggplot(matriculados, aes(x=Grade_Typology, y=Amount)) + geom_bar(stat = "identity") + coord_flip() + labs(title="Student Distribution by Grade Typology")+
+geom_text(aes(y=Amount, label=cantidad_estudiantes), vjust=1.5, color="white", size=3.5, hjust="right")
