c5caff9d07cff6bc85c5049a2cbb2102125c451d
Regina del Pilar Berges Portillo <170300066@ucaribe.edu.mx>
2020-02-23 17:24:36 -0500

    Actualización de examen

--- parcial1/examen/examen.R
+++ parcial1/examen/examen.R
@@ -1,1 +1,5 @@
-# kk
+# 1. Exploraci´on y visualizaci´on de variables:
+#i. Exploren la base de datos, realice los cambios necesarios y convenientes para poder trabajar con ella.
+
+setwd("C:/Users/regina/Desktop/6to Semestre/Minería de datos/portafolioJairo/parcial1/examen")
+df <- read.csv("Ejercicio 1.csv", sep = ",")
